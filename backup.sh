1223edb29b52ce480c5aa8bdbf5d86065d7d2b82
RGBboy <me@rgbboy.com>
2013-01-22 11:50:28 -0500

    fixed calling script from outside;

--- backup.sh
+++ backup.sh
@@ -65,6 +65,9 @@ then
   exit 1
 fi
 
+# Get the directory the script is being run from
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo $DIR
 # Store the current date in YYYY-mm-DD-HHMMSS
 DATE=$(date -u "+%F-%H%M%S")
 FILE_NAME="backup-$DATE"
@@ -75,21 +78,21 @@ ARCHIVE_NAME="$FILE_NAME.tar.gz"
 mongo -username "$MONGODB_USER" -password "$MONGODB_PASSWORD" admin --eval "var databaseNames = db.getMongo().getDBNames(); for (var i in databaseNames) { printjson(db.getSiblingDB(databaseNames[i]).getCollectionNames()) }; printjson(db.fsyncLock());"
 
 # Dump the database
-mongodump -username "$MONGODB_USER" -password "$MONGODB_PASSWORD" --out $FILE_NAME
+mongodump -username "$MONGODB_USER" -password "$MONGODB_PASSWORD" --out $DIR/$FILE_NAME
 
 # Unlock the database
 mongo -username "$MONGODB_USER" -password "$MONGODB_PASSWORD" admin --eval "printjson(db.fsyncUnlock());"
 
 # Tar Gzip the file
-tar -zcvf ./backup/$ARCHIVE_NAME $FILE_NAME
+tar -zcvf $DIR/backup/$ARCHIVE_NAME $DIR/$FILE_NAME
 
 # Remove the backup directory
-rm -r $FILE_NAME
+rm -r $DIR/$FILE_NAME
 
 # Send the file to the backup drive or S3
 
 HEADER_DATE=$(date -u "+%a, %d %b %Y %T %z")
-CONTENT_MD5=$(openssl dgst -md5 -binary ./backup/$ARCHIVE_NAME | openssl enc -base64)
+CONTENT_MD5=$(openssl dgst -md5 -binary $DIR/backup/$ARCHIVE_NAME | openssl enc -base64)
 CONTENT_TYPE="application/x-download"
 STRING_TO_SIGN="PUT\n$CONTENT_MD5\n$CONTENT_TYPE\n$HEADER_DATE\n/$S3_BUCKET/$ARCHIVE_NAME"
 SIGNATURE=$(echo -e -n $STRING_TO_SIGN | openssl dgst -sha1 -binary -hmac $AWS_SECRET_KEY | openssl enc -base64)
@@ -100,5 +103,5 @@ curl -X PUT \
 --header "content-type: $CONTENT_TYPE" \
 --header "Content-MD5: $CONTENT_MD5" \
 --header "Authorization: AWS $AWS_ACCESS_KEY:$SIGNATURE" \
---upload-file ./backup/$ARCHIVE_NAME \
+--upload-file $DIR/backup/$ARCHIVE_NAME \
 https://$S3_BUCKET.s3-ap-southeast-1.amazonaws.com/$ARCHIVE_NAME
