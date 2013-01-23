61ef9f58ae88e646cc01834ee2a2f86efabca410
RGBboy <me@rgbboy.com>
2013-01-23 10:22:45 -0500

    fixed dump directory;

--- backup.sh
+++ backup.sh
@@ -80,16 +80,16 @@ ARCHIVE_NAME="$FILE_NAME.tar.gz"
 mongo -username "$MONGODB_USER" -password "$MONGODB_PASSWORD" admin --eval "var databaseNames = db.getMongo().getDBNames(); for (var i in databaseNames) { printjson(db.getSiblingDB(databaseNames[i]).getCollectionNames()) }; printjson(db.fsyncLock());"
 
 # Dump the database
-mongodump -username "$MONGODB_USER" -password "$MONGODB_PASSWORD" --out $DIR/$FILE_NAME
+mongodump -username "$MONGODB_USER" -password "$MONGODB_PASSWORD" --out $DIR/backup/$FILE_NAME
 
 # Unlock the database
 mongo -username "$MONGODB_USER" -password "$MONGODB_PASSWORD" admin --eval "printjson(db.fsyncUnlock());"
 
 # Tar Gzip the file
-tar -C $DIR/backup/ -zcvf $DIR/backup/$ARCHIVE_NAME $FILE_NAME
+tar -C $DIR/backup/ -zcvf $DIR/backup/$ARCHIVE_NAME $FILE_NAME/
 
 # Remove the backup directory
-rm -r $DIR/$FILE_NAME
+rm -r $DIR/backup/$FILE_NAME
 
 # Send the file to the backup drive or S3
 
